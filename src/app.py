886d0118f40733eb92330e443b9a3a551d1e92db
KingMuerte <alberto.nieto.80@gmail.com>
2023-04-27 22:06:29 -0700

    Add execute_python_code func - Include Name exception handling - Include Syntax exception handling - Add appropriate pytests

--- src/app.py
+++ src/app.py
@@ -1,8 +1,12 @@
+import ast
+import builtins
+
 from dotenv import load_dotenv, find_dotenv
 from langchain.utilities import WikipediaAPIWrapper
+from langchain.utilities import PythonREPL
+
 
 load_dotenv(find_dotenv())
-
 
 def run_wiki(search_term: str) -> str:
     """
@@ -17,10 +21,76 @@ def run_wiki(search_term: str) -> str:
     if not search_term:
         return ""
 
+    # create an instance of WikipediaAPIWrapper and call its run method with the search term
     wikipedia = WikipediaAPIWrapper()
     result = wikipedia.run(search_term)
+
     return result
 
 
+def execute_python_code(code: str) -> str:
+    """
+    Executes the given Python code using an instance of PythonREPL.
+
+    Args:
+        code: (str) The string containing the Python code to execute.
+
+    Returns:
+        str: The returned output from executing the code.
+    """
+    if not code:
+        return ""
+
+    try:
+        # compile the code to bytecode
+        compiled_code = compile(code, "<string>", "exec")
+    except SyntaxError as e:
+        # raise a SyntaxError if the code is not valid Python code
+        raise e
+
+    # create a visitor object to traverse the abstract syntax tree of the compiled code
+    class VariableNameVisitor(ast.NodeVisitor):
+        def __init__(self):
+            self.names = set()
+
+        def visit_Name(self, node):
+            self.names.add(node.id)
+
+        def visit_FunctionDef(self, node):
+            self.names.add(node.name)
+
+    # parse the code into an abstract syntax tree and traverse it with the visitor object
+    tree = ast.parse(code)
+    visitor = VariableNameVisitor()
+    visitor.visit(tree)
+    variable_names = visitor.names
+
+    # Check for undefined variables
+    local_namespace = locals()
+    undefined_variables = [
+        name
+        for name in variable_names
+        if name not in local_namespace and name not in dir(builtins)
+    ]
+    if undefined_variables:
+        # Raise a new NameError with a custom message
+        raise NameError(f"Undefined variables: {', '.join(undefined_variables)}")
+
+    # create an instance of PythonREPL and call its run method with the compiled code
+    python_repl = create_python_repl()
+    return python_repl.run(code)
+
+
+def create_python_repl() -> PythonREPL:
+    """
+    Creates and returns an instance of PythonREPL.
+    """
+    # create an instance of PythonREPL and return it
+    return PythonREPL()
+
 if __name__ == "__main__":
-    print(run_wiki("Mexico"))
+    try:
+        print(execute_python_code("print(13+2)"))
+        print(run_wiki("Birria"))
+    except Exception as e:
+        print(e)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,8 @@
-from src.app import run_wiki
+import pytest
+from src.app import run_wiki, execute_python_code
 
 
-def test_search_for_python():
+def test_wiki_search_for_python():
     """
     This function tests if run_wiki('Python') returns a string with length greater than 0.
     """
@@ -10,7 +11,7 @@ def test_search_for_python():
     assert len(result) > 0
 
 
-def test_search_for_ai():
+def test_wiki_search_for_ai():
     """
     This function tests if run_wiki('Artificial intelligence') returns a string with length greater than 0.
     """
@@ -19,10 +20,32 @@ def test_search_for_ai():
     assert len(result) > 0
 
 
-def test_search_for_empty_string():
+def test_wiki_search_for_empty_string():
     """
     This function tests if run_wiki('') returns an empty string.
     """
     result = run_wiki("")
     assert isinstance(result, str)
     assert len(result) == 0
+
+def test_execute_python_code_with_valid_code():
+    code = "print('Hello, World!')"
+    expected_output = "Hello, World!\n"
+    assert execute_python_code(code) == expected_output
+
+
+def test_execute_python_code_with_empty_string():
+    code = ""
+    assert execute_python_code(code) == ""
+
+
+def test_execute_python_code_with_invalid_syntax():
+    code = "print('Hello, World!'"
+    with pytest.raises(SyntaxError):
+        execute_python_code(code)
+
+
+def test_execute_python_code_with_name_error():
+    code = "print(variable_that_does_not_exist)"
+    with pytest.raises(NameError):
+        execute_python_code(code)
